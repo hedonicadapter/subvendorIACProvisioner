694c685ba98eeab10f62461383a47e3abc187e45
hedonicadapter <mailservice.samherman@gmail.com>
2025-05-10 20:04:44 +0200

    var

--- function_app.py
+++ function_app.py
@@ -7,6 +7,9 @@ from python_terraform import *
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+iacDir = "/tmp/IAC"
+tf = Terraform(working_dir=iacDir)
+
 # 1. validate request data with openapi and apim
 def validateRequest(req: func.HttpRequest):
    raise NotImplementedError("not implemented")
@@ -16,24 +19,25 @@ def cloneIACRepo():
     repo_url = "https://github.com/hedonicadapter/terraform-modules.git"
     logging.info(f"Cloning repository from {repo_url}.")
 
-    repo = Repo.clone_from(repo_url, "/tmp/IAC")
+    repo = Repo.clone_from(repo_url, iacDir)
     logging.info(f"Repository cloned successfully: {repo}.")
 
 def initializeDirectory():
     logging.info("Removing existing IAC directory.")
-    _ = subprocess.run(["rm", "-rf", "/tmp/IAC"])
+    _ = subprocess.run(["rm", "-rf", iacDir])
 
     logging.info("Upserting tmp IAC directory")
-    os.makedirs("/tmp/IAC", exist_ok=True)
+    os.makedirs(iacDir, exist_ok=True)
 
 # 3. provision IAC
+def terraformInit():
+    tf.init()
+
 def terraformPlan():
-    raise NotImplementedError("Terraform plan not implemented")
+    tf.plan()
 
 def terraformApply():
-    # tf = Terraform(working_dir='./IAC', variables={'a':'b', 'c':'d'})
-    # tf.apply()
-    raise NotImplementedError("Terraform plan not implemented")
+    tf.apply()
 
 
 @app.route(route="createSubscription")
@@ -43,6 +47,7 @@ def createSubscription(req: func.HttpRequest) -> func.HttpResponse:
         # validateRequest(req)
         initializeDirectory()
         cloneIACRepo()
+        # terraformInit()
         # terraformPlan()
         # terraformApply()
 
