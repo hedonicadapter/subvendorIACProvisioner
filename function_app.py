9f5903e14bca7eab30a676e3a300166ac182d1ed
hedonicadapter <mailservice.samherman@gmail.com>
2025-05-10 08:55:17 +0200

    write to tmp

--- function_app.py
+++ function_app.py
@@ -11,12 +11,12 @@ def createSubscription(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("createSubscription function triggered.")
     try:
         logging.info("Removing existing IAC directory.")
-        subprocess.run(["rm", "-rf", "./IAC"])
+        subprocess.run(["rm", "-rf", "/tmp/IAC"])
 
         repo_url = "https://github.com/hedonicadapter/terraform-modules.git"
         logging.info(f"Cloning repository from {repo_url}.")
 
-        repo = Repo.clone_from(repo_url, "./IAC")
+        repo = Repo.clone_from(repo_url, "/tmp/IAC")
         logging.info(f"Repository cloned successfully: {repo}.")
 
         return func.HttpResponse(
