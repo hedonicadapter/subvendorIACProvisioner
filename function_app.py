d2b18ecd2f91837429016941bff50c0403fc6096
hedonicadapter <mailservice.samherman@gmail.com>
2025-05-10 08:23:47 +0200

    logging

--- function_app.py
+++ function_app.py
@@ -1,27 +1,37 @@
 import azure.functions as func
 import subprocess
 import logging
-
 from git import Repo
 from python_terraform import *
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-# validate request data with openapi and apim
-
-# run terraform init && terraform plan
-# tf = Terraform(working_dir='./IAC', variables={'a':'b', 'c':'d'})
-# tf.apply()
-
 @app.route(route="createSubscription")
 def createSubscription(req: func.HttpRequest) -> func.HttpResponse:
-    # clone IAC repo
-    subprocess.run(["rm", "-rf", "./IAC"]) 
-    repo_url = "https://github.com/hedonicadapter/terraform-modules.git"
-    repo = Repo.clone_from(repo_url, "./IAC")
-    print(repo)
+    logging.info("createSubscription function triggered.")
+    try:
+        logging.info("Removing existing IAC directory.")
+        subprocess.run(["rm", "-rf", "./IAC"])
 
-    return func.HttpResponse(
-         "This HTTP triggered function executed successfully.",
-         status_code=200
-    )
+        repo_url = "https://github.com/hedonicadapter/terraform-modules.git"
+        logging.info(f"Cloning repository from {repo_url}.")
+
+        repo = Repo.clone_from(repo_url, "./IAC")
+        logging.info(f"Repository cloned successfully: {repo}.")
+
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully.",
+            status_code=200
+        )
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Subprocess error: {e}")
+        return func.HttpResponse(
+            "Internal Server Error: Subprocess failed.",
+            status_code=500
+        )
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return func.HttpResponse(
+            f"Internal Server Error: {str(e)}",
+            status_code=500
+        )
