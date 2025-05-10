7beee936b0ba7f0a4c71454857ae4b179cb3256a
hedonicadapter <mailservice.samherman@gmail.com>
2025-05-10 08:03:22 +0200

    clone

--- function_app.py
+++ function_app.py
@@ -8,16 +8,16 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 # validate request data with openapi and apim
 
-# clone IAC repo
-# repo_url = "https://github.com/hedonicadapter/terraform-modules.git"
-# repo = Repo.clone_from(repo_url, "./IAC")
-
 # run terraform init && terraform plan
 # tf = Terraform(working_dir='./IAC', variables={'a':'b', 'c':'d'})
 # tf.apply()
 
 @app.route(route="createSubscription")
 def createSubscription(req: func.HttpRequest) -> func.HttpResponse:
+    # clone IAC repo
+    repo_url = "https://github.com/hedonicadapter/terraform-modules.git"
+    repo = Repo.clone_from(repo_url, "./IAC")
+    print(repo)
     return func.HttpResponse(
          "This HTTP triggered function executed successfully.",
          status_code=200
